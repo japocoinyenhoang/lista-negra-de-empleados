d1ab8dddc81ebbd8c80f5bd84f7d37cfc366be40
yen <japocoin@gmail.com>
2019-03-26 19:00:29 +0100

    finish video 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import { getPersons } from './services/personService';
 class App extends Component {
+  constructor (props){
+    super (props);
+    
+    this.state={
+      results: this.getSavedData()
+
+
+    }
+  }
+
+  getSavedData (){
+    const blackData = localStorage.getItem('blackData');
+    if (blackData !== null) {
+      return JSON.parse(blackData);
+    } else {
+      this.getBlackCriaturas();
+      return [];
+    }
+  }
+
+  saveData (data) {
+    localStorage.setItem('blackData', JSON.stringify(data));
+  }
+
+  getBlackCriaturas (){
+    getPersons()
+    .then (data => {
+      const cleanData = data.results.map((item, index) => {return {...item, id:index}});
+      this.setState({
+        results: cleanData
+      });
+      this.saveData(cleanData);
+    }
+  )}
+
   render() {
     return (
-      <div className="App">
-        ;P
+      <div className="app">
+      <h1 className="app__title">Lista negra de empleados</h1>
+        <ul className="app__list">
+        {this.state.results.map(item => {
+          return (
+            <li className="app__list-item" key={item.id}>
+              <div className="person">
+                <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
+                <img src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`} className="person__image" />
+                <div className="person__age">{item.dob.age}</div>
+                <div className="person__city">{item.location.city}</div>
+              </div>
+            </li>
+          )
+        })}
+        </ul>
       </div>
     );
   }
